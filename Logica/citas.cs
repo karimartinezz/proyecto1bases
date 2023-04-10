efd16f038ca262cf5ca4cb5837ac8b4d8cba104d
karimartinezz <103463090+karimartinezz@users.noreply.github.com>
2023-04-09 21:40:46 -0600

    Add files via upload

--- Logica/citas.cs
+++ Logica/citas.cs
@@ -12,14 +12,34 @@ namespace Logica
         Datos.ConexionBD conexion = new Datos.ConexionBD();
 
        
-        public void registrarCita(string observaciones, string area, string fechaSeleccionada, TimeSpan horaSeleccionada)
+        public void registrarCita(string observaciones, string area, string fechaSeleccionada, TimeSpan horaSeleccionada, int cedula)
         {
            
             string estado = "Registrada";
             int id = conexion.generarID("Cita" , "ID");
-            conexion.registrarCitaDAO(id, observaciones, area, horaSeleccionada, fechaSeleccionada,  estado);
+            conexion.registrarCitaDAO(id, observaciones, area, horaSeleccionada, fechaSeleccionada,  estado, cedula);
 
+        }
+        public int revisarPaciente(int cedula)
+        {
+            int validar=conexion.validarExistenciaPaciente(cedula);
+            return validar;
 
+        }
+        public int revisarCita(int cedula, string fechaSeleccionada, TimeSpan horaSeleccionada) {
+            int validar=conexion.validarPacienteCita( cedula,  fechaSeleccionada,  horaSeleccionada);
+            return validar;
+        }
+        public void cancelarCita(int ID)
+        {
+            string estado = "Cancelada por paciente";
+            conexion.cancelarCitaDAO(ID, estado);
+
+        }
+        public void cancelarCitaCentro(int ID)
+        {
+            string estado = "Cancelada por centro medico";
+            conexion.cancelarCitaDAO(ID, estado);
 
         }
 
